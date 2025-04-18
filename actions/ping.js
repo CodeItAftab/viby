d712c9b5ea3e44a7dbc05cf251dc2e268ccd8613
Aftab Alam <aftabalamdlm@gmail.com>
2025-04-18 22:56:07 +0530

    added an action to keep server alive@v3

--- actions/ping.js
+++ actions/ping.js
@@ -3,6 +3,17 @@ const https = require("https");
 https
   .get("https://viby.onrender.com", (res) => {
     console.log(`Status Code: ${res.statusCode}`);
+
+    let data = "";
+
+    res.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    res.on("end", () => {
+      console.log("Response Body:", data);
+      process.exit(0); // Clean exit
+    });
   })
   .on("error", (e) => {
     console.error(`Error: ${e.message}`);
